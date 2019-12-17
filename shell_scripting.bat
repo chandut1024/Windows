4bc57fd9b083a01df9f420a09ffb8a188f3af86c
chandut1024 <58700836+chandut1024@users.noreply.github.com>
2019-12-17 20:24:14 +0530

    version1

---
+++ shell_scripting.bat
@@ -0,0 +1,11 @@
+@echo off
+:: Open folders
+REM start %USERPROFILE%\Documents\
+start %USERPROFILE%\Desktop\ETLFiles\
+:: Open files
+REM start chrome.exe
+start "" "C:\Users\chandu.t\Downloads\putty.exe"
+start "" "%windir%\system32\cmd.exe"
+start "" "%USERPROFILE%\Desktop\ETLFiles\Commands.txt"
+REM start "" "%USERPROFILE%\Documents\test_jan2011.csv"
+exit
---
+++ windows.bat
@@ -0,0 +1,14 @@
+@echo off
+:: Open folders
+REM start %USERPROFILE%\Documents\
+REM start %USERPROFILE%\Desktop\ETLFiles\
+:: Open files
+REM start chrome.exe
+start %USERPROFILE%\Downloads\Katalon_Studio_Windows_64-5.8.2\katalon.exe
+start %USERPROFILE%\Desktop\pageant.exe
+REM start %USERPROFILE%\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe"
+start "" "C:\Program Files\Microsoft Office\Office16\outlook.exe"
+REM start "" "%USERPROFILE%\Documents\test_jan2011.csv"
+start %USERPROFILE%\Desktop\Chandu\Katalon\LoginDetailsNJS.xlsx
+REM start "" "C:\Program Files (x86)\Notepad++\notepad++.exe"
+exit
